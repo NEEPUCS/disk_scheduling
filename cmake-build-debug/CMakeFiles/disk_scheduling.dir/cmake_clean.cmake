31719474370c533bd4261fd34525ed304f4591f0
李鸿钊 <583971499@qq.com>
2019-12-01 18:59:35 +0800

    First Commit

---
+++ cmake-build-debug/CMakeFiles/disk_scheduling.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/disk_scheduling.dir/main.cpp.o"
+  "disk_scheduling"
+  "disk_scheduling.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/disk_scheduling.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
